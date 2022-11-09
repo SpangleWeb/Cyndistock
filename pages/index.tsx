93f9c0c191e5d808989a6ddfcb7b831bf79a7922
Mitch Boland <mitch.boland@nutrien.com.au>
2022-11-09 17:48:03 +1000

    refactor(page): removed console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line prettier/prettier
 import prisma from "../lib/prisma";
 import { TestComponent } from "../components/testComponent";
-import { useMe } from "../lib/hooks";
+// import { useMe } from "../lib/hooks";
 
 const Home = ({ artists }) => {
-  const { user } = useMe();
+  // const { user } = useMe();
   // console.log("hello", artists);
-  console.log("what is the user " + JSON.stringify(user));
   return <TestComponent data={artists} />;
 };
 
