28fe1e428ec7fefd2e4485817d9f4630c2e5f53f
Mitch Boland <88764061+MitchBoland@users.noreply.github.com>
2022-11-10 19:58:56 +1000

    feat(authFormStyles): updated the form styles to resolve size bug (#5)

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -9,30 +9,26 @@ const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
 
   return (
     <Flex
-      height="100vh"
       width="100vw"
-      // bgGradient="linear(to-l, #2998ff, #5643fa)"
+      bgGradient="linear(to-l, #2998ff, #5643fa)"
       color="white"
       justifyContent="center"
       alignItems="center"
+      minHeight="100vh"
     >
       <Flex
         justify="center"
         align="center"
-        // height="100px"
-        // borderBottom="1px solid white"
-        flexDirection="row"
+        marginTop="24px"
+        marginBottom="24px"
       >
-        {/* <NextImage src="/logo.svg" alt="site logo" width={120} height={60} /> */}
-      </Flex>
-      <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box
           padding="50px"
           bg="white"
           borderRadius="6px"
           width="400px"
           maxWidth="calc(100vw - 48px)"
-          borderTop=" 8px solid #5643fa"
+          // borderTop=" 8px solid #5643fa"
           boxShadow="0px 3px 9px rgba(0, 0, 0, 0.4)"
         >
           <Flex
